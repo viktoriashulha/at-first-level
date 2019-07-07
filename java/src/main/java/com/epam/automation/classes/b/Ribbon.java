d8123900bbd038105c34d742e6f072a368d6f3a7
Viktoria Shulha <viktoria.shulha@gmail.com>
2019-07-07 19:38:06 +0300

    Revert "all tasks"
    This reverts commit 22041c1030f6458b0fa5a67f181e1f66f9aa2bc2.

--- java/src/main/java/com/epam/automation/classes/b/Ribbon.java
+++
@@ -1,9 +0,0 @@
-package com.epam.automation.classes.b;
-
-public class Ribbon extends Accessory{
-
-   public Ribbon(){
-        super(1);
-    }
-
-}
--- java/src/main/java/com/epam/automation/exceptions/EmptyFileException.java
+++
@@ -1,7 +0,0 @@
-package com.epam.automation.exceptions;
-
-public class EmptyFileException extends Exception {
-    public EmptyFileException(String message) {
-        super(message);
-    }
-}
